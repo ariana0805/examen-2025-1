ea30467681313b99b32287f5cc5684e22c6b6712
O23235 <O23235@lab.aca>
2025-04-28 21:44:47 -0500

    ari

--- ejercicio3/index.js
+++ ejercicio3/index.js
@@ -1,19 +1,19 @@
-const verificar = function () {
+let verificacion = function () {
     let texto1 = document.getElementById("texto1")
     let texto2 = document.getElementById("texto2")
     let selector=document.getElementById("carrera")
     let otro=document.getElementById("vacio")
-    if(texto1.value=="" || texto2.value=="" || selector.value=="Selecciona una opción..."){
-        console.error(  "Error, debe llenar todos los campos")
-        otro.className="fondorojo"
+    if(texto1.value=="" || texto2.value=="" || selector.value=="Selecciona una opción"){
+        console.error("Error, debe llenar todos los campos")
+        otro.className="fondorosa"
         otro.textContent="Error, debe llenar todos los campos"
         return
     }
     console.log("Se guardo con exito")
-    otro.className="fondoverde"
-    otro.textContent="Se guardo con exito"
+    otro.className="fondomorado"
+    otro.textContent="Se guardó con exito"
 }
-const main = function () {
+let main = function () {
     document.getElementById("boton").addEventListener("click", verificar)
 }
 
